2da0e30543650496d1fbc22c7618c739f112145f
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-01-14 06:32:54 +0900

    Delete old backend files

--- bblogback/src/main/java/dev/hyns/bblogback/Repository/ArticleImageRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.hyns.bblogback.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.hyns.bblogback.Entity.ArticleImage;
-
-public interface ArticleImageRepository extends JpaRepository<ArticleImage, Long>{
-    
-}
--- bblogback/src/main/java/dev/hyns/bblogback/Repository/HashtagRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.hyns.bblogback.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.hyns.bblogback.Entity.Hashtag;
-
-public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-    
-}
