017b1c540e84f22d175e0fe80eaf608fc9a90732
Hyunseok Byun <gumyoincirno@gmail.com>
2023-01-24 15:34:16 +0900

    font update

--- bblogfrontv2nextjs/pages/_app.tsx
+++ bblogfrontv2nextjs/pages/_app.tsx
@@ -6,17 +6,27 @@ import { Header } from "../Components/Header/Header";
 import wrapper from "../Store/store";
 import "./index.sass";
 
+import { Hahmlet, Saira } from "@next/font/google";
+const hahmlet = Hahmlet({ subsets: ["latin"] });
+const saira = Saira({ subsets: ["latin"] });
 const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
     const { store, props } = wrapper.useWrappedStore(rest);
     return (
-        <Provider store={store}>
-            <ChakraProvider>
-                <Container maxW="1024px" minWidth="400px" marginBottom="2rem" p={0} lang={"ko"}>
-                    <Header />
-                    <Component {...props.pageProps} />
-                </Container>
-            </ChakraProvider>
-        </Provider>
+        <>
+            <style jsx global>{`
+                * {
+                    font-family: ${saira.style.fontFamily}, ${hahmlet.style.fontFamily}, sans-serif;
+                }
+            `}</style>
+            <Provider store={store}>
+                <ChakraProvider>
+                    <Container maxW="1024px" minWidth="400px" marginBottom="2rem" p={0}>
+                        <Header />
+                        <Component {...props.pageProps} />
+                    </Container>
+                </ChakraProvider>
+            </Provider>
+        </>
     );
 };
 export default MyApp;
