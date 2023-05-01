fcc3e322ae67ec521212d814c7373c0937706c1f
Hyunseok Byun <gumyoincirno@gmail.com>
2023-05-02 01:30:09 +0900

    Fix issue for main page

--- bblogv3/src/pages/index.tsx
+++ bblogv3/src/pages/index.tsx
@@ -6,42 +6,40 @@ import dayjs from "dayjs";
 import dompurify from "isomorphic-dompurify";
 import Head from "next/head";
 
-export async function getStaticProps() {
-    const { data } = await axios.post("https://hyns.dev/v1/article/list", { aid: -1, menu: "INTRO" });
-    const { article } = data;
-
+export const getStaticProps = async () => {
+    const { data } = await axios.post("https://hyns.dev/v1/article/read", { aid: 1 });
     return {
-        props: { props: { article } }, // will be passed to the page component as props
+        props: { article: data },
     };
-}
+};
 
 interface homeProps {
-    props: { article: article };
+    article: article;
 }
 
-const Home = ({ props }: homeProps) => {
+const Home = ({ article }: homeProps) => {
     const sanitizer = dompurify.sanitize;
     return (
         <>
             <Head>
                 <title>HS :: Intro</title>
-                <meta name="description" content={`${props.article.context.replace(/<[^>]+>/g, "")}`} />
+                <meta name="description" content={`${article.context.replace(/<[^>]+>/g, "")}`} />
                 <meta name="keywords" content="Developer homepage, Intropage, Resume" />
                 <meta property="og:type" content="blog" />
                 <meta property="og:url" content="https://hyns.dev" />
                 <meta property="og:title" content="HS :: Intro" />
                 <meta property="og:image" content="/favicon.ico" />
-                <meta property="og:description" content={`${props.article.context.replace(/<[^>]+>/g, "")}`} />
+                <meta property="og:description" content={`${article.context.replace(/<[^>]+>/g, "")}`} />
                 <meta property="og:site_name" content="Hyunseok" />
                 <meta property="og:locale" content="ko_KR" />
             </Head>
-            <Box className="" borderWidth="1px" borderColor={'#303030'} p={3} mt={5}>
+            <Box className="" borderWidth="1px" borderColor={"#303030"} p={3} mt={5}>
                 <Flex direction={"column"} gap={3}>
                     <Flex alignItems={"center"} gap={1}>
                         <Icon icon="uil:calender" />
-                        <span className="text-sm text-gray-500 translate-y-[0.5px]">{dayjs(props.article.createdDate).locale("ko").format("YYYY년 MM월 DD일")}</span>
+                        <span className="text-sm text-gray-500 translate-y-[0.5px]">{dayjs(article.createdDate).locale("ko").format("YYYY MM  DD")}</span>
                     </Flex>
-                    <div dangerouslySetInnerHTML={{ __html: sanitizer(props.article.context) }}></div>
+                    <div dangerouslySetInnerHTML={{ __html: sanitizer(article.context) }}></div>
                 </Flex>
             </Box>
         </>
