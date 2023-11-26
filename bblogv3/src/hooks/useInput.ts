820e612798cdda666c0effbe9e6e29296a2e338c
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-26 23:04:43 +0900

    [FRONT] Remove old files

--- bblogv3/src/hooks/useInput.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent, useCallback, useState } from "react";
-
-const useInput = (val = "") => {
-    const [value, setValue] = useState<any>(val);
-    const onChange = useCallback((e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => setValue(e.currentTarget.value), []);
-    return [value, onChange, setValue] as const;
-};
-
-export default useInput;
