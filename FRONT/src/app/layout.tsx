247dbeeff6d965fbb063e9637cbeff9ad90b1b67
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-27 21:15:11 +0900

    [FRONT] Fix master layout

--- FRONT/src/app/layout.tsx
+++ FRONT/src/app/layout.tsx
@@ -9,13 +9,13 @@ import './globals.css'
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
         <html lang='ko' suppressHydrationWarning>
-            <body className={cn('min-h-screen bg-background font-mplusrounded antialiased flex flex-col')}>
+            <body className={cn('min-h-screen bg-primary-foreground font-mplusrounded antialiased flex flex-col')}>
                 <ThemeProvider attribute='class'>
-                    <Header className='border-b' />
+                    <Header />
                     <Flex className='justify-start p-0 h-full flex-1'>
-                        <Sidebar className='min-h-full pt-14 shadow-xl' />
-                        <Flex className='p-0 pt-14 flex-col transition-all md:ml-[285px] w-full shadow-xl'>
-                            <section className='p-5'>{children}</section>
+                        <Sidebar className='min-h-full pt-[3.75rem]' />
+                        <Flex className='p-0 pt-[3.75rem] flex-col transition-all md:ml-[285px] w-full'>
+                            <section className='p-5  bg-background'>{children}</section>
                             <Flex className='flex-col items-center justify-center border-t h-10'>Copyright © 2023. BLOGOWNER</Flex>
                         </Flex>
                     </Flex>
