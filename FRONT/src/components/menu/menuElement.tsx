063bb63c3560895b3910d0ea3817990069b7f61c
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-27 21:16:30 +0900

    [FRONT] Styling menu buttons

--- FRONT/src/components/menu/menuElement.tsx
+++ FRONT/src/components/menu/menuElement.tsx
@@ -8,14 +8,21 @@ type menuElementProps = {
     count: string | number
 } & HTMLAttributes<HTMLElement>
 
-const menuElement = ({ className, title, count }: menuElementProps) => {
+const MenuElement = ({ className, title, count }: menuElementProps) => {
     const isActivated = false
     return (
-        <Flex className={cn(`ml-3 gap-1 items-baseline ${!isActivated ? 'opacity-50' : ''}`, className)}>
+        <Flex
+            className={cn(
+                `pl-5 gap-1 items-baseline cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800 transition-all rounded 
+                ${!isActivated ? 'opacity-50' : ''}
+                `,
+                className,
+            )}
+        >
             <span className='text-primary'> - {title}</span>
             <span className='text-xs'>({count})</span>
         </Flex>
     )
 }
 
-export default menuElement
+export default MenuElement
