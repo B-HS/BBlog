e50c031b10d5e0d7beb6cf6225c916e89840d3a2
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-27 21:14:28 +0900

    [FRONT] Resize profile section

--- FRONT/src/components/profile/profile.tsx
+++ FRONT/src/components/profile/profile.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 const Profile = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
     const markdownText = `STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100`
     return (
-        <Flex className={cn('max-h-56 flex-col justify-center items-center gap-2 mt-5', className)} {...props}>
+        <Flex className={cn('max-h-72 h-72 flex-col justify-center items-center gap-5', className)} {...props}>
             <Avatar className='h-28 w-28'>
                 <AvatarImage src='' alt='USERNAME' />
                 <AvatarFallback>USER NAME</AvatarFallback>
