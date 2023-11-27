fabd07df34ecc543b289a5ab751087faef1b8768
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-27 21:14:18 +0900

    [FRONT] Clean up side bar

--- FRONT/src/components/sidebar/sidebar.tsx
+++ FRONT/src/components/sidebar/sidebar.tsx
@@ -6,13 +6,11 @@ import Profile from '../profile/profile'
 const Sidebar = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
     return (
         <Flex
-            className={cn('md:flex md:min-w-[280px] md:max-w-[280px] p-0 flex-col gap-1 fixed w-0 overflow-hidden transition-all', className)}
+            className={cn('md:flex md:min-w-[280px] md:max-w-[280px] p-0 flex-col gap-1.5 fixed w-0 overflow-hidden transition-all', className)}
             {...props}
         >
-            <Profile className='shadow' />
-            <div className='shadow rotate-180'>
-                <Menu className='rotate-180' style={{}} />
-            </div>
+            <Profile className='bg-background' />
+            <Menu className='bg-background flex-1' />
         </Flex>
     )
 }
