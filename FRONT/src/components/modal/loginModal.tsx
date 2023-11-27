773b26c6afa20019a1d564f1a2fd9d80af77c446
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-27 21:16:51 +0900

    [FRONT] Fix login modal

--- FRONT/src/components/modal/loginModal.tsx
+++ FRONT/src/components/modal/loginModal.tsx
@@ -5,16 +5,18 @@ import { Label } from '@/components/ui/label'
 import { GithubIcon, UserPlus2Icon } from 'lucide-react'
 import Flex from '../flex'
 import { Separator } from '../ui/separator'
+import ForgotPassword from './forgotPasswordModal'
+import SignIn from './joinModal'
 const LoginModal = ({ children }: React.HTMLAttributes<HTMLElement>) => {
     return (
         <Dialog>
             <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent className='sm:max-w-[425px]'>
+            <DialogContent className='sm:max-w-[425px] gap-1.5'>
                 <DialogHeader>
                     <DialogTitle>Login</DialogTitle>
                     <DialogDescription></DialogDescription>
                 </DialogHeader>
-                <div className='grid gap-4 py-4'>
+                <div className='grid gap-4'>
                     <div className='grid grid-cols-4 items-center gap-4'>
                         <Label htmlFor='name' className='text-right'>
                             Email
@@ -29,21 +31,27 @@ const LoginModal = ({ children }: React.HTMLAttributes<HTMLElement>) => {
                     </div>
                 </div>
                 <DialogFooter>
-                    <Flex className='p-0 justify-between gap-2 w-full'>
-                        <Button variant={'ghost'}>Forgot password</Button>
-                        <Button type='submit'>Login</Button>
+                    <Flex className='p-0 justify-end gap-2 w-full'>
+                        <ForgotPassword>
+                            <Button variant={'ghost'}>Forgot password</Button>
+                        </ForgotPassword>
                     </Flex>
                 </DialogFooter>
                 <Separator />
-                <Flex className='flex-col gap-2'>
+                <Flex className='flex-col gap-2 mt-3'>
+                    <Button type='submit' className='bg-neutral-500'>
+                        Login
+                    </Button>
                     <Button>
                         <GithubIcon className='mr-1' />
                         Log in with Github
                     </Button>
-                    <Button size='default' variant={'outline'}>
-                        <UserPlus2Icon className='mr-1' />
-                        Sign in
-                    </Button>
+                    <SignIn>
+                        <Button size='default' variant={'outline'}>
+                            <UserPlus2Icon className='mr-1' />
+                            Sign in
+                        </Button>
+                    </SignIn>
                 </Flex>
             </DialogContent>
         </Dialog>
