186c5b814b590255ad4cf9935e7453d331cb569a
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-30 00:01:07 +0900

    [BACK] Update exception for approaching

--- BACK/src/main/java/net/gumyo/bblog/security/WebConfig.java
+++ BACK/src/main/java/net/gumyo/bblog/security/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
-        // localhost, 127.0.0.1 맵핑, 프로덕트로 올라가면 어차피 동일 선상에 올릴 것이기에 아래 두개만 개발요으로 허용
+        // localhost, 127.0.0.1 맵핑, 프로덕트로 올라가면 어차피 동일 선상에 올릴 것이기에 아래 두개만 개발용으로 허용
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
