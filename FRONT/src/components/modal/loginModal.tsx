a7b85ff3361ae25a8c03aed1245d1e00725987fd
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:17:09 +0900

    [FRONT] Rename/fix the filename by file change

--- FRONT/src/components/modal/loginModal.tsx
+++ FRONT/src/components/modal/loginModal.tsx
@@ -1,4 +1,4 @@
-import { userLogin } from '@/api/user/login'
+import { userLogin } from '@/api/user/user'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
