d166196d54c26e508d04b0507d809b894e2c3715
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:55:38 +0900

    [FRONT] Add title for collapsible sidebar

--- FRONT/src/components/sidebar/collapsibleSidebar.tsx
+++ FRONT/src/components/sidebar/collapsibleSidebar.tsx
@@ -1,10 +1,12 @@
+import { adminProfile } from '@/api/user/user'
 import { MenuIcon } from 'lucide-react'
 import Menu from '../menu/menu'
 import Profile from '../profile/profile'
 import { Button } from '../ui/button'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet'
 
-const CollapsibleSidebar = () => {
+const CollapsibleSidebar = async () => {
+    const bloginfo = await adminProfile(true)
     return (
         <Sheet>
             <SheetTrigger asChild>
@@ -15,7 +17,7 @@ const CollapsibleSidebar = () => {
             <SheetContent side={'left'} className='min-w-[280px] w-full p-0'>
                 <SheetHeader>
                     <SheetTitle className='text-left p-2.5'>
-                        <span className='text-xl font-bold whitespace-nowrap'>BLOGTITLE</span>
+                        <span className='text-xl font-bold whitespace-nowrap'>{bloginfo.title}</span>
                     </SheetTitle>
                     <SheetDescription asChild>
                         <section>
