b6811de07860479407ffe8517fae3c39a04e9941
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:20:06 +0900

    [FRONT] Set initial theme for theme switch

--- FRONT/src/module/modules/theme.tsx
+++ FRONT/src/module/modules/theme.tsx
@@ -1,9 +1,12 @@
 'use client'
-
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    return (
+        <NextThemesProvider enableSystem={false} {...props}>
+            {children}
+        </NextThemesProvider>
+    )
 }
 export default ThemeProvider
