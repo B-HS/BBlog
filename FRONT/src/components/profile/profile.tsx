278bd2df350af221556e51384cc4b3a66f0db4e6
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:17:26 +0900

    [FRONT] Add profile info

--- FRONT/src/components/profile/profile.tsx
+++ FRONT/src/components/profile/profile.tsx
@@ -1,16 +1,21 @@
+import { adminProfile } from '@/api/user/user'
 import { cn } from '@/lib/utils'
 import Flex from '../flex'
+import UpdownAnime from '../transition/updown'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
-const Profile = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
-    const markdownText = `STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100STRING_LENGH_MAX_100`
+const Profile = async ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
+    const bloginfo = await adminProfile(true)
+
     return (
         <Flex className={cn('max-h-72 h-72 flex-col justify-center items-center gap-5', className)} {...props}>
-            <Avatar className='h-28 w-28'>
-                <AvatarImage src='' alt='USERNAME' />
-                <AvatarFallback>USER NAME</AvatarFallback>
-            </Avatar>
-            <p className='text-center break-all' dangerouslySetInnerHTML={{ __html: markdownText }}></p>
+            <UpdownAnime className='flex flex-col justify-center items-center gap-5'>
+                <Avatar className='h-[12rem] w-[12rem]'>
+                    <AvatarImage src='https://avatars.githubusercontent.com/u/49316060' alt={bloginfo.nickname} />
+                    <AvatarFallback>{bloginfo.nickname}</AvatarFallback>
+                </Avatar>
+                <p className='text-center break-all' dangerouslySetInnerHTML={{ __html: bloginfo.introduce ? bloginfo.introduce : '' }}></p>
+            </UpdownAnime>
         </Flex>
     )
 }
