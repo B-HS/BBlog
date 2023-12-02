ba857739ee2a5f9d66ae9965f0f5d0a84703a7db
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:14:50 +0900

    [FRONT] Add loading blogname/subtitle

--- FRONT/src/components/header/header.tsx
+++ FRONT/src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import { adminProfile } from '@/api/user/user'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import Flex from '../flex'
@@ -7,15 +8,16 @@ import Search from './icons/search'
 import Theme from './icons/theme'
 import User from './icons/user'
 
-const Header = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
+const Header = async ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
+    const bloginfo = await adminProfile(true)
     return (
         <nav className={cn('fixed w-full bg-background z-50 flex justify-between items-center space-x-4 lg:space-x-6 h-14', className)} {...props}>
             <UpdownAnime>
                 <Flex className='gap-2 items-center'>
                     <CollapsibleSidebar />
-                    <Link href='/' className='flex items-baseline gap-2 p-0'>
-                        <span className='text-lg font-bold whitespace-nowrap'>BLOGTITLE</span>
-                        <span className='text-[0.7rem] opacity-70'>SUBTITLE</span>
+                    <Link href='/' className='flex items-baseline p-0 gap-1'>
+                        <span className='text-xl font-bold whitespace-nowrap'>{bloginfo.title}</span>
+                        <span className='text-[0.7rem] opacity-70'>{bloginfo.subtitle}</span>
                     </Link>
                 </Flex>
             </UpdownAnime>
