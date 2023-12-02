92646aace4b5866debfacda7b54dd290a5d1a577
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 02:49:57 +0900

    [BACK] Add basic route generating

--- BACK/src/test/java/net/gumyo/bblog/BblogApplicationTests.java
+++ BACK/src/test/java/net/gumyo/bblog/BblogApplicationTests.java
@@ -1,5 +1,6 @@
 package net.gumyo.bblog;
 
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import net.gumyo.bblog.entity.Menu;
 import net.gumyo.bblog.entity.User;
 import net.gumyo.bblog.entity.User.Role;
+import net.gumyo.bblog.repository.MenuRepository;
 import net.gumyo.bblog.repository.UserRepository;
 
 @SpringBootTest
@@ -17,6 +20,8 @@ class BblogApplicationTests {
 	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private UserRepository urepo;
+	@Autowired
+	private MenuRepository mrepo;
 
 	@Test
 	void createUser() {
@@ -36,6 +41,27 @@ class BblogApplicationTests {
 				.roles(Set.of(Role.ADMIN, Role.USER))
 				.build());
 		urepo.save(user);
+	}
+
+	@Test
+	void createMenu() {
+		List<Menu> menus = List.of(
+				createMenu("dev", 0L),
+				createMenu("front", 1L),
+				createMenu("back", 1L),
+				createMenu("travel", 0L),
+				createMenu("etc", 3L));
+		mrepo.saveAll(menus);
+	}
+
+	private Menu createMenu(String menuname, Long parentmekey) {
+		return Menu.builder()
+				.mename(menuname)
+				.parentmekey(parentmekey)
+				.meorder(0)
+				.hide(false)
+				.icon(null)
+				.build();
 	}
 
 }
