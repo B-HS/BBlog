de6ca22eb6a3e3f7d2dfad93970575b22e3257d3
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-03 03:19:08 +0900

    [FRONT] Fix loading screen style

--- FRONT/src/components/loading.tsx
+++ FRONT/src/components/loading.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { LoadingContext } from '@/module/context/loading'
 import { Loader2Icon } from 'lucide-react'
 import { useContext } from 'react'
@@ -6,9 +7,10 @@ import Flex from './flex'
 
 const Loading = () => {
     const { isLoading } = useContext(LoadingContext)
+
     return (
         isLoading && (
-            <Flex className='absolute z-[9999] w-full h-full items-center justify-center bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 backdrop-blur-sm'>
+            <Flex className='fixed z-[9999] w-full h-full items-center justify-center backdrop-blur-sm transition-all'>
                 <Loader2Icon className='animate-spin' />
             </Flex>
         )
