466d7bc01d792dc64c28d8a9dbbe92fe79e38584
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-04 00:03:43 +0900

    [FRONT] Add delay for profile

--- FRONT/src/components/profile/profile.tsx
+++ FRONT/src/components/profile/profile.tsx
@@ -3,13 +3,14 @@ import { cn } from '@/lib/utils'
 import Flex from '../flex'
 import UpdownAnime from '../transition/updown'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { DELAY } from '@/lib/constant'
 
 const Profile = async ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
     const bloginfo = await adminProfile(true)
 
     return (
         <Flex className={cn('max-h-72 h-72 flex-col justify-center items-center gap-5', className)} {...props}>
-            <UpdownAnime className='flex flex-col justify-center items-center gap-5'>
+            <UpdownAnime className='flex flex-col justify-center items-center gap-5' delay={DELAY(1)}>
                 <Avatar className='h-[12rem] w-[12rem]'>
                     <AvatarImage src='https://avatars.githubusercontent.com/u/49316060' alt={bloginfo.nickname} />
                     <AvatarFallback>{bloginfo.nickname}</AvatarFallback>
