3ac50bf306ed50509ef352f14fb97ed1659faec3
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-04 00:03:26 +0900

    [FRONT] Fix updown transition for add delay

--- FRONT/src/components/transition/updown.tsx
+++ FRONT/src/components/transition/updown.tsx
@@ -4,7 +4,7 @@ import { useTheme } from 'next-themes'
 import dynamic from 'next/dynamic'
 const MotionDiv = dynamic(() => import('framer-motion').then((mod) => mod.motion.div), { ssr: false })
 
-const UpdownAnime = ({ children, className }: React.HTMLAttributes<HTMLElement>) => {
+const UpdownAnime = ({ children, className, delay = 0 }: React.PropsWithChildren<{ className?: string; delay?: number }>) => {
     const { theme } = useTheme()
     return MotionDiv ? (
         <MotionDiv
@@ -12,7 +12,7 @@ const UpdownAnime = ({ children, className }: React.HTMLAttributes<HTMLElement>)
             key={theme}
             initial={{ y: -20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.2 }}
+            transition={{ duration: 0.2, delay }}
         >
             {children}
         </MotionDiv>
