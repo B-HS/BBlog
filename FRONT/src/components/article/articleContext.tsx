426762a0c4df53038f8d2bf5cbf2b32a196d3890
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-06 01:40:41 +0900

    [FRONT] Fix article context component

--- FRONT/src/components/article/articleContext.tsx
+++ FRONT/src/components/article/articleContext.tsx
@@ -1,12 +1,24 @@
-import { Article } from '@/app/[category]/[aid]/page'
 import dayjs from 'dayjs'
 import Flex from '../flex'
 import { Badge } from '../ui/badge'
 import { Separator } from '../ui/separator'
 
+export interface Article {
+    aid: number
+    title: string
+    context: string
+    tags: string[]
+    mekey: number
+    fileseq: null | number
+    hide: boolean
+    github: null | string
+    publishlink: null | string
+    insertDate: null | string
+}
+
 const ArticleContext = ({ data, category }: { data: Article; category: string }) => {
     return (
-        <Flex className='posting flex-col flex-none w-full'>
+        <Flex className='posting flex-col flex-none w-full '>
             <header className='flex justify-between items-baseline'>
                 <Flex className='gap-2 items-baseline'>
                     <span className='text-5xl'>{data.title}</span>
@@ -18,14 +30,14 @@ const ArticleContext = ({ data, category }: { data: Article; category: string })
                 </Flex>
             </header>
             <Separator className='my-3' />
-            <Flex>
+            <div className='p-10' dangerouslySetInnerHTML={{ __html: data.context }}></div>
+            <Flex className='p-0 gap-2 flex-wrap'>
                 {data.tags.map((tag, idx) => (
-                    <Badge variant='outline' className='rounded' key={idx}>
+                    <Badge variant='outline' className='rounded py-2 dark:shadow-neutral-900 shadow-md' key={idx}>
                         {tag.toUpperCase()}
                     </Badge>
                 ))}
             </Flex>
-            <div className='p-10' dangerouslySetInnerHTML={{ __html: data.context }}></div>
         </Flex>
     )
 }
