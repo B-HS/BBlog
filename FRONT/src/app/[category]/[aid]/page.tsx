50f7baf614e848209bb084c4cba82bd2c2cd586b
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-06 01:40:20 +0900

    [FRONT] Add comment component

--- FRONT/src/app/[category]/[aid]/page.tsx
+++ FRONT/src/app/[category]/[aid]/page.tsx
@@ -1,32 +1,57 @@
 import { getPostById } from '@/api/article/post'
+import ArticleComement, { Comment } from '@/components/article/articleComment'
 import ArticleContext from '@/components/article/articleContext'
 import Flex from '@/components/flex'
 import { Separator } from '@/components/ui/separator'
 
-export interface Article {
-    aid: number
-    title: string
-    context: string
-    tags: string[]
-    mekey: number
-    fileseq: null | number
-    hide: boolean
-    github: null | string
-    publishlink: null | string
-    insertDate: null | string
-}
-
 const Article = async ({ params: { aid, category } }: { params: { aid: string; category: string } }) => {
-    const data: Article = await getPostById(aid, true)
+    const dummyCommentList = [
+        {
+            aid: 2,
+            cid: 1,
+            img: 'https://picsum.photos/200/300',
+            commentorder: 1,
+            nickname: 'guest1',
+            context: 'comment 1',
+            uppercid: null,
+            insertdate: '20231201000000',
+        },
+        {
+            aid: 2,
+            cid: 2,
+            nickname: 'guest2',
+            commentorder: 1,
+            img: 'https://picsum.photos/200/300',
+            context: 'comment 1 - reply',
+            uppercid: 1,
+            insertdate: '20231201000000',
+        },
+        {
+            aid: 2,
+            cid: 3,
+            nickname: 'admin',
+            commentorder: 2,
+            img: 'https://picsum.photos/200/300',
+            context: 'comment2',
+            uppercid: null,
+            insertdate: '20231201000000',
+        },
+    ] as Comment[]
+
+    const data = await getPostById(aid, true)
     return (
-        <Flex className='flex-col flex-wrap justify-start max-w-5xl w-full'>
-            {ArticleContext({ data, category })}
+        <Flex className='flex-col flex-wrap justify-start max-w-6xl w-full'>
+            <ArticleContext data={data} category={category} />
             <Separator className='my-5' />
-            <Flex className='flex-col'>
-                <header className='flex gap-1 items-baseline'>
+            <Flex className='flex-col gap-5'>
+                <header className='flex gap-1 items-baseline mb-5'>
                     <p className='text-2xl'>Comment</p>
                     <span className='text-xs opacity-80'>({0})</span>
                 </header>
+                {/* TODO 계층형으로 데이터 가공해서 댓글-대댓글 구현 필요 */}
+                {dummyCommentList.map((comment, idx) => (
+                    <ArticleComement key={idx} data={comment} />
+                ))}
             </Flex>
         </Flex>
     )
---
+++ FRONT/src/components/article/articleComment.tsx
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs'
+import { MessageSquareIcon, MoreHorizontalIcon } from 'lucide-react'
+import Flex from '../flex'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Button } from '../ui/button'
+
+export interface Comment {
+    cid: number
+    aid: number
+    nickname: string
+    img: string
+    context: string
+    commentorder: number
+    uppercid: null | number
+    insertdate: null | string
+}
+
+const ArticleComement = ({ data }: { data: Comment }) => {
+    return (
+        <Flex className='flex-col border dark:shadow-neutral-900 shadow-lg'>
+            <Flex className='gap-2 items-center'>
+                <Avatar className='border'>
+                    <AvatarImage src={data.img ? data.img : '/favicon.ico'} alt='usericon' />
+                    <AvatarFallback>{data.nickname.split(' ').length === 1 ? data.nickname.slice(0, 2) : data.nickname}</AvatarFallback>
+                </Avatar>
+                <Flex className='gap-2 items-baseline -translate-y-0.5'>
+                    <span className='text-lg font-bold'>{data.nickname}</span>
+                    <span className='text-sm opacity-50'>{dayjs(data.insertdate, { format: 'YYYYMMDDHHmmss' }).format('YYYY. MM. DD. HH:mm')}</span>
+                </Flex>
+            </Flex>
+            <section className='px-3.5 py-2'>
+                <p>{data.context}</p>
+            </section>
+            <Flex className='p-1.5 gap-2'>
+                <Button size={'icon'} variant={'ghost'}>
+                    <MessageSquareIcon />
+                </Button>
+                <Button size={'icon'} variant={'ghost'}>
+                    <MoreHorizontalIcon />
+                </Button>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default ArticleComement
