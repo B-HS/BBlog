e611e2f7bae3b59b662392999b9b576d51906823
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-09 20:35:32 +0900

    [FRONT] Fix post api logic

--- FRONT/src/api/article/post.ts
+++ FRONT/src/api/article/post.ts
@@ -10,10 +10,16 @@ export const getPostById = async (id: number | string, isServerside?: boolean) =
     }
 }
 
-export const getAllPost = async (isServerside?: boolean) => {
+export const getAllPost = async (isServerside?: boolean, onlyCount?: boolean) => {
     try {
         if (isServerside) axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACK
         const { data } = await axios.get('/article/list')
+        if (onlyCount) {
+            return data.reduce((prev: { [x: string]: number }, next: { mekey: string | number }) => {
+                prev[`ME_${next.mekey}`] = (prev[`ME_${next.mekey}`] || 0) + 1
+                return prev
+            }, {})
+        }
         return data
     } catch (error) {
         return error
