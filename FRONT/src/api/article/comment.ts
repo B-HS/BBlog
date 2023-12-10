a6aaa659e7edc66e9292fad549daae1bcc5a4b11
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-10 18:37:29 +0900

    [FRONT] Add logic for comment CUD

--- FRONT/src/api/article/comment.ts
+++ FRONT/src/api/article/comment.ts
@@ -1,82 +1,37 @@
+import { Comment } from '@/components/article/comment'
 import axios from 'axios'
 
 export const getCommentByAid = async (aid: string | number, isServerside?: boolean) => {
-    return [
-        {
-            aid: 2,
-            cid: 1,
-            img: 'https://picsum.photos/200/300',
-            commentorder: 1,
-            nickname: 'guest1',
-            context: 'comment 1',
-            uppercid: 0,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 2,
-            nickname: 'guest2',
-            commentorder: 1,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment 1 - reply',
-            uppercid: 1,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 3,
-            nickname: 'admin',
-            commentorder: 2,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment2',
-            uppercid: 1,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 4,
-            nickname: 'guest3',
-            commentorder: 2,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment2',
-            uppercid: 0,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 5,
-            nickname: 'guest4',
-            commentorder: 3,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment3',
-            uppercid: 0,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 6,
-            nickname: 'guest5',
-            commentorder: 3,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment1 - reply2',
-            uppercid: 1,
-            insertdate: '20231201000000',
-        },
-        {
-            aid: 2,
-            cid: 7,
-            nickname: 'guest6',
-            commentorder: 0,
-            img: 'https://picsum.photos/200/300',
-            context: 'comment2 - reply3',
-            uppercid: 4,
-            insertdate: '20231201000000',
-        },
-    ]
-    // 임시 코멘트
     try {
         if (isServerside) axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACK
-        const { data } = await axios.get('/comment/' + aid)
+        const { data } = await axios.get('/comment/list/' + aid)
+        return data
+    } catch (error) {
+        return error
+    }
+}
+
+export const saveComment = async (comment: Partial<Comment>) => {
+    try {
+        const { data } = await axios.post('/comment/save', comment)
+        return data
+    } catch (error) {
+        return error
+    }
+}
+
+export const deleteComment = async (comment: Partial<Comment>) => {
+    try {
+        const { data } = await axios.post('/comment/remove', comment)
+        return data
+    } catch (error) {
+        return error
+    }
+}
+
+export const updateComment = async (comment: Partial<Comment>) => {
+    try {
+        const { data } = await axios.post('/comment/modify', comment)
         return data
     } catch (error) {
         return error
