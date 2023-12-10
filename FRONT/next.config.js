da9303e40bf0d5c2bb93169f6a11bc66b6cfc4d3
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-10 18:36:38 +0900

    [FRONT] Add images remotePatterns

--- FRONT/next.config.js
+++ FRONT/next.config.js
@@ -4,7 +4,17 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
     // Configure `pageExtensions` to include MDX files
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    // Optionally, add any other Next.js config below
+    // Optionally, add any other Next.js config below,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.gumyo.net',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 }
 
 module.exports = withMDX(nextConfig)
