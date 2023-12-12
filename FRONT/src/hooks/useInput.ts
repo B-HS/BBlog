02893879c9b8d58ef634adeaab279626bc2c7051
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-12 20:25:55 +0900

    [FRONT] Add custom hook

---
+++ FRONT/src/hooks/useInput.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent, useCallback, useState } from 'react'
+
+const useInput = (val = '') => {
+    const [value, setValue] = useState<any>(val)
+    const onChange = useCallback((e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => setValue(e.currentTarget.value), [])
+    return [value, onChange, setValue] as const
+}
+
+export default useInput
