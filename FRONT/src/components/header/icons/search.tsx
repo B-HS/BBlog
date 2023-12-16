4c2ee781330d2c9d281d51c6aa4e071d149de81e
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-16 22:21:08 +0900

    [FRONT] Implement post search

--- FRONT/src/components/header/icons/search.tsx
+++ FRONT/src/components/header/icons/search.tsx
@@ -1,11 +1,46 @@
 'use client'
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
+import { getAllpostByKeyword } from '@/api/article/post'
+import { loadMenu } from '@/api/menu/menu'
+import { Article } from '@/components/article/articleContext'
+import Flex from '@/components/flex'
+import { Command, CommandInput } from '@/components/ui/command'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { SearchIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { buttonVariants } from '../../ui/button'
 const Search = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isOnSearch, setIsOnSearch] = useState(false)
+    const [timeoutList, setTimeoutList] = useState<NodeJS.Timeout[]>([])
+    const [posts, setPosts] = useState<Article[]>([])
+    const router = useRouter()
+    const redirectToPost = (post: Article) => {
+        router.push(`/${post.category?.toLowerCase() || 'post'}/${post.aid}`)
+        setIsOpen(false)
+    }
+
+    const searchEvent = async (keyword: string) => {
+        timeoutList.forEach((ele) => clearTimeout(ele))
+        setTimeoutList([])
+        setIsOnSearch(true)
+        const timeout = setTimeout(async () => {
+            if (keyword === '') {
+                setIsOnSearch(false)
+                return
+            }
+            const menus = await loadMenu()
+            const posts = await getAllpostByKeyword(keyword)
+            const remappedPosts = posts.map((ele: { mekey: any }) => ({
+                ...ele,
+                category: menus.find((me: { mekey: any }) => me.mekey === ele.mekey)!.mename,
+            }))
+            setPosts(() => [...remappedPosts])
+            setIsOnSearch(false)
+        }, 500)
+        setTimeoutList(() => [timeout])
+    }
+
     return (
         <Popover open={isOpen} onOpenChange={setIsOpen}>
             <PopoverTrigger className={buttonVariants({ variant: 'ghost', size: 'icon' })}>
@@ -13,40 +48,27 @@ const Search = () => {
             </PopoverTrigger>
             <PopoverContent className='p-0 sm:w-[639px] transition-all'>
                 <Command>
-                    <CommandInput placeholder='Keyword...' />
-                    <CommandList>
-                        <CommandEmpty>No results found.</CommandEmpty>
-                        <CommandGroup heading='Suggestions'>
-                            <CommandItem
-                                className='cursor-pointer'
-                                onSelect={(e) => {
-                                    console.log(e as string)
-                                    setIsOpen(false)
-                                }}
-                            >
-                                <span>11</span>
-                            </CommandItem>
-                            <CommandItem
-                                className='cursor-pointer'
-                                onSelect={(e) => {
-                                    console.log(e as string)
-                                    setIsOpen(false)
-                                }}
-                            >
-                                <span>22</span>
-                            </CommandItem>
-                            <CommandItem
-                                className='cursor-pointer'
-                                onSelect={(e) => {
-                                    console.log(e as string)
-                                    setIsOpen(false)
-                                }}
-                            >
-                                <span>3</span>
-                            </CommandItem>
-                        </CommandGroup>
-                        <CommandSeparator />
-                    </CommandList>
+                    <CommandInput onValueChange={searchEvent} />
+
+                    <Flex className='flex-col p-0 divide-y-2'>
+                        {isOnSearch && (
+                            <Flex className='p-3 w-full h-full justify-center items-center'>
+                                <SearchIcon className='animate-bounce' />
+                            </Flex>
+                        )}
+                        {!isOnSearch &&
+                            posts.map((ele, idx) => (
+                                <Flex
+                                    onClick={() => redirectToPost(ele)}
+                                    key={idx}
+                                    className='cursor-pointer px-3 hover:bg-neutral-200 dark:hover:bg-neutral-800 transition-all'
+                                >
+                                    <Flex className='p-0 flex-1 justify-start gap-2 items-center'>
+                                        <span>{ele.title}</span> <span className='opacity-50 text-xs'>- {ele.category}</span>
+                                    </Flex>
+                                </Flex>
+                            ))}
+                    </Flex>
                 </Command>
             </PopoverContent>
         </Popover>
