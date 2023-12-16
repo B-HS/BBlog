6cf177fbada5e0d9ab94d15c637164941fb9f7f1
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-16 22:19:09 +0900

    [FRONT] Fix post category logic

--- FRONT/src/components/article/articleCard.tsx
+++ FRONT/src/components/article/articleCard.tsx
@@ -1,19 +1,18 @@
 import dayjs from 'dayjs'
 import { CalendarIcon, Layers3Icon } from 'lucide-react'
 import Link from 'next/link'
-import { useParams } from 'next/navigation'
 import Flex from '../flex'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Badge } from '../ui/badge'
 import { Article } from './articleContext'
 
 const ArticleCard = ({ article }: { article: Article }) => {
-    const { aid, title, context, tags, insertDate } = article
-    const param = useParams()
+    const { aid, title, context, tags, insertDate, category: rawCategory } = article
+    const category = rawCategory?.toLowerCase()
 
     return (
         <Flex className='border w-full p-0 overflow-hidden lg:h-[266px] flex-col lg:flex-row hover:shadow-lg dark:shadow-neutral-900 transition-all hover:border-2'>
-            <Link className='h-100 w-100 aspect-video lg:aspect-square rounded-none' href={`/${param.category}/${aid}`}>
+            <Link className='h-100 w-100 aspect-video lg:aspect-square rounded-none' href={`/${category}/${aid}`}>
                 <Avatar className='h-100 w-100 aspect-video lg:aspect-square rounded-none'>
                     <AvatarImage className='object-fill' src='https://picsum.photos/200/300' />
                     <AvatarFallback className='lg:aspect-square rounded-none'>{`ARTICLE_${aid}_LOGO`}</AvatarFallback>
@@ -24,18 +23,18 @@ const ArticleCard = ({ article }: { article: Article }) => {
                     <Flex className='gap-2 opacity-50 items-center flex-wrap'>
                         <Flex className='gap-2 items-center p-0'>
                             <Layers3Icon className='w-3.5' />
-                            <span>{(param.category as string).toUpperCase()}</span>
+                            <span>{(category as string).toUpperCase()}</span>
                         </Flex>
                         <Flex className='gap-2 items-center p-0'>
                             <CalendarIcon className='w-3.5' />
                             <span>{dayjs(insertDate, { format: 'YYYYMMDDHHmmss' }).format('YYYY. MM. DD. HH:mm')}</span>
                         </Flex>
                     </Flex>
-                    <Link href={`/${param.category}/${aid}`}>
+                    <Link href={`/${category}/${aid}`}>
                         <span className='pl-3 font-bold text-3xl'>{title}</span>
                     </Link>
                     <Flex className='flex-col overflow-ellipsis h-[9.75rem] lg:h-auto p-5'>
-                        <Link className='h-100 w-100 aspect-video lg:aspect-square rounded-none' href={`/${param.category}/${aid}`}>
+                        <Link className='h-100 w-100 aspect-video lg:aspect-square rounded-none' href={`/${category}/${aid}`}>
                             {context
                                 .replace(/<\/?[^>]+(>|$)/g, '')
                                 .replace(/&nbsp;/g, ' ')
