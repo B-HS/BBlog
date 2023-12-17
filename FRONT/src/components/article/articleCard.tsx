25f9609c6c0cc3194a9ce734a6198e41613c596e
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-17 18:36:17 +0900

    [FRONT] Fix typo and replace img to get from loaded data

--- FRONT/src/components/article/articleCard.tsx
+++ FRONT/src/components/article/articleCard.tsx
@@ -7,14 +7,14 @@ import { Badge } from '../ui/badge'
 import { Article } from './articleContext'
 
 const ArticleCard = ({ article }: { article: Article }) => {
-    const { aid, title, context, tags, insertDate, category: rawCategory } = article
+    const { aid, title, context, tags, insertdate, category: rawCategory, thumbnail } = article
     const category = rawCategory?.toLowerCase()
 
     return (
         <Flex className='border w-full p-0 overflow-hidden lg:h-[266px] flex-col lg:flex-row hover:shadow-lg dark:shadow-neutral-900 transition-all hover:border-2'>
             <Link className='h-100 w-100 aspect-video lg:aspect-square rounded-none' href={`/${category}/${aid}`}>
                 <Avatar className='h-100 w-100 aspect-video lg:aspect-square rounded-none'>
-                    <AvatarImage className='object-fill' src='https://picsum.photos/200/300' />
+                    <AvatarImage className='object-fill' src={thumbnail ? `https://img.gumyo.net/${thumbnail}` : 'favicon.ico'} />
                     <AvatarFallback className='lg:aspect-square rounded-none'>{`ARTICLE_${aid}_LOGO`}</AvatarFallback>
                 </Avatar>
             </Link>
@@ -27,7 +27,7 @@ const ArticleCard = ({ article }: { article: Article }) => {
                         </Flex>
                         <Flex className='gap-2 items-center p-0'>
                             <CalendarIcon className='w-3.5' />
-                            <span>{dayjs(insertDate, { format: 'YYYYMMDDHHmmss' }).format('YYYY. MM. DD. HH:mm')}</span>
+                            <span>{dayjs(insertdate, { format: 'YYYYMMDDHHmmss' }).format('YYYY. MM. DD. HH:mm')}</span>
                         </Flex>
                     </Flex>
                     <Link href={`/${category}/${aid}`}>
