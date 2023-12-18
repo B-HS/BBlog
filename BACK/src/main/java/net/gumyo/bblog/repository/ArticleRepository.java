fa5a62977109e6720ff606abd074c7fdb8338d76
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-18 20:33:40 +0900

    [BACK] Rollback article repository

--- BACK/src/main/java/net/gumyo/bblog/repository/ArticleRepository.java
+++ BACK/src/main/java/net/gumyo/bblog/repository/ArticleRepository.java
@@ -1,13 +1,10 @@
 package net.gumyo.bblog.repository;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import net.gumyo.bblog.entity.Article;
 
@@ -19,18 +16,5 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Article findByAidAndHideFalse(Long aid);
 
     List<Article> findAllByTitleLikeOrContextLikeAndHideFalse(String title, String context);
-
-    @Query("SELECT SUBSTRING(s.insertdate, 1, 8) AS dateGroup, " +
-            "s.params, " +
-            "s.method, " +
-            "MAX(s.ipv6) AS ipv6, " +
-            "s.ipv4, " +
-            "MAX(s.insertdate) AS insertdate " +
-            "FROM SystemLog s " +
-            "WHERE s.method = 'getArticle' " +
-            "AND SUBSTRING(s.insertdate, 1, 8) BETWEEN SUBSTRING(:startDate, 1, 8) AND  SUBSTRING(:endDate, 1, 8) " +
-            "GROUP BY dateGroup, s.params, s.ipv4 " +
-            "ORDER BY dateGroup, s.params, s.ipv4")
-    List<Map<String, Object>> getCustomResult(@Param("startDate") String startDate, @Param("endDate") String endDate);
 
 }
