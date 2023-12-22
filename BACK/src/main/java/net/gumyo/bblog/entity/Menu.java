61c0f94d9e8effada1045f5958a6d9311b93b260
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-23 03:58:30 +0900

    [BACK] Add Page type and adjust generate template

--- BACK/src/main/java/net/gumyo/bblog/entity/Menu.java
+++ BACK/src/main/java/net/gumyo/bblog/entity/Menu.java
@@ -39,7 +39,7 @@ public class Menu {
     private MenuType type;
 
     public enum MenuType {
-        PAGE, LIST
+        PAGE, LIST, INTRODUCE
     }
 
 }
--- BACK/src/test/java/net/gumyo/bblog/BblogApplicationTests.java
+++ BACK/src/test/java/net/gumyo/bblog/BblogApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import net.gumyo.bblog.entity.Article;
 import net.gumyo.bblog.entity.Menu;
+import net.gumyo.bblog.entity.Menu.MenuType;
 import net.gumyo.bblog.entity.User;
 import net.gumyo.bblog.entity.User.Role;
 import net.gumyo.bblog.repository.ArticleRepository;
@@ -50,19 +51,34 @@ class BblogApplicationTests {
 	@Test
 	void createMenu() {
 		List<Menu> menus = List.of(
-				createMenu("Dev", 0L),
-				createMenu("Frontend", 1L),
-				createMenu("Backend", 1L),
-				createMenu("etc.", 0L),
-				createMenu("Travel", 4L));
+				createMenu("Introduce", 0L, MenuType.PAGE),
+				createMenu("Dev", 0L, MenuType.LIST),
+				createMenu("Frontend", 2L, MenuType.LIST),
+				createMenu("Backend", 2L, MenuType.LIST),
+				createMenu("etc.", 0L, MenuType.LIST),
+				createMenu("Travel", 5L, MenuType.LIST),
+				createMenu("React", 3L, MenuType.LIST));
 		mrepo.saveAll(menus);
 	}
 
 	@Test
 	void createArticle() {
-		arepo.saveAll(List.of(createArticle("test", new String[] { "test" }, "test"),
+		arepo.saveAll(List.of(
+				createArticle("test", new String[] { "test" }, "test"),
 				createArticle("test2", new String[] { "test2" }, "test2"),
-				createArticle("test3", new String[] { "test3" }, "test3")));
+				createArticle("test3", new String[] { "test3" }, "test3"),
+				createArticle("test4", new String[] { "test4" }, "test4"),
+				createArticle("test5", new String[] { "test5" }, "test5"),
+				createArticle("test6", new String[] { "test6" }, "test6"),
+				createArticle("test7", new String[] { "test7" }, "test7"),
+				createArticle("test8", new String[] { "test8" }, "test8"),
+				createArticle("test9", new String[] { "test9" }, "test9"),
+				createArticle("test10", new String[] { "test10" }, "test10"),
+				createArticle("test11", new String[] { "test11" }, "test11"),
+				createArticle("test12", new String[] { "test12" }, "test12"),
+				createArticle("test13", new String[] { "test13" }, "test13"),
+				createArticle("test14", new String[] { "test14" }, "test14"),
+				createArticle("test15", new String[] { "test15" }, "test15")));
 
 	}
 
@@ -76,13 +92,14 @@ class BblogApplicationTests {
 				.build();
 	}
 
-	private Menu createMenu(String menuname, Long parentmekey) {
+	private Menu createMenu(String menuname, Long parentmekey, MenuType type) {
 		return Menu.builder()
 				.mename(menuname)
 				.parentmekey(parentmekey)
 				.meorder(0)
 				.hide(false)
 				.icon(null)
+				.type(type)
 				.build();
 	}
 
