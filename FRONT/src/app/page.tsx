0e4dd4943f5db2b9ad5b207a5332410b1f72ed22
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-23 11:21:56 +0900

    [FRONT] Add mainpage with sanitizing/meta data

--- FRONT/src/app/page.tsx
+++ FRONT/src/app/page.tsx
@@ -1,6 +1,37 @@
-'use client'
-const Home = () => {
-    return <>EMPTY</>
+import { getArticleByMenuName } from '@/api/article/post'
+import { Article } from '@/components/article/articleContext'
+import DOMPurify from 'isomorphic-dompurify'
+
+export const generateMetadata = async ({ params: { docid } }: { params: { docid: string } }) => {
+    const article = (await getArticleByMenuName('MAIN', true)) as Article
+    const context = DOMPurify.sanitize(article.context || '').replace(/<\/?[^>]+(>|$)/g, '')
+
+    return {
+        title: 'Hyunseok Byun - BBlog',
+        description: context || 'Article | BBlog',
+        keywords: article.tags?.join(', ') || 'Article | BBlog',
+        openGraph: {
+            title: article.title || 'Article | BBlog',
+            description: context || 'Article | BBlog',
+            images: article.thumbnail ? [`https://img.gumyo.net/${article.thumbnail}`] : undefined,
+        },
+        twitter: {
+            title: article.title || 'Article | BBlog',
+            description: context || 'Article | BBlog',
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        canonical: `https://gumyo.net/doc/${docid}`,
+        authors: { name: 'Hyunoseok Byun', url: 'https://gumyo.net' },
+        icons: { icon: '/favicon.ico' },
+    }
+}
+
+const Home = async () => {
+    const main = (await getArticleByMenuName('MAIN', true)) as Article
+    return <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(main.context || '') }} />
 }
 
 export default Home
