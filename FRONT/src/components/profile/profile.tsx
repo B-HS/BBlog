df0c5febd2cec140742f15b755e7d59104b5390c
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-23 11:27:13 +0900

    [FRONT] Add sanitize for profile context

--- FRONT/src/components/profile/profile.tsx
+++ FRONT/src/components/profile/profile.tsx
@@ -4,9 +4,11 @@ import { cn } from '@/lib/utils'
 import Flex from '../flex'
 import UpdownAnime from '../transition/updown'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import DOMPurify from 'isomorphic-dompurify'
 
 const Profile = async ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
     const bloginfo = await adminProfile(true)
+    const context = DOMPurify.sanitize(bloginfo.introduce || '')
     return (
         <Flex className={cn('max-h-72 h-72 flex-col justify-center items-center gap-5', className)} {...props}>
             <UpdownAnime className='flex flex-col justify-center items-center gap-5' delay={DELAY(1)}>
@@ -17,7 +19,7 @@ const Profile = async ({ className, ...props }: React.HTMLAttributes<HTMLElement
                     />
                     <AvatarFallback>{bloginfo.nickname}</AvatarFallback>
                 </Avatar>
-                <p className='text-center break-all' dangerouslySetInnerHTML={{ __html: bloginfo.introduce ? bloginfo.introduce : '' }}></p>
+                <p className='text-center break-all' dangerouslySetInnerHTML={{ __html: context }}></p>
             </UpdownAnime>
         </Flex>
     )
