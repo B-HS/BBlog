18f3c95dcae8fe889d9181a6940f04b5be0e9682
Hyunseok Byun <gumyoincirno@gmail.com>
2023-12-23 11:23:03 +0900

    [FRONT] Set santiize/metadata for post page

--- FRONT/src/app/[category]/[aid]/page.tsx
+++ FRONT/src/app/[category]/[aid]/page.tsx
@@ -3,9 +3,37 @@ import ArticleContext from '@/components/article/articleContext'
 import CommentList from '@/components/article/commentList'
 import Flex from '@/components/flex'
 import { Separator } from '@/components/ui/separator'
+import DOMPurify from 'isomorphic-dompurify'
+
+export const generateMetadata = async ({ params: { aid, category } }: { params: { aid: string; category: string } }) => {
+    const article = (await getPostById(aid, true)) || {}
+    const context = DOMPurify.sanitize(article.context || '').replace(/<\/?[^>]+(>|$)/g, '')
+
+    return {
+        title: article.title || 'Article | BBlog',
+        description: context || 'Article | BBlog',
+        keywords: article.tags?.join(', ') || 'Article | BBlog',
+        openGraph: {
+            title: article.title || 'Article | BBlog',
+            description: context || 'Article | BBlog',
+            images: article.thumbnail ? [`https://img.gumyo.net/${article.thumbnail}`] : undefined,
+        },
+        twitter: {
+            title: article.title || 'Article | BBlog',
+            description: context || 'Article | BBlog',
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        canonical: `https://gumyo.net/${category}/${aid}`,
+        authors: { name: 'Hyunoseok Byun', url: 'https://gumyo.net' },
+        icons: { icon: '/favicon.ico' },
+    }
+}
 
 const Article = async ({ params: { aid, category } }: { params: { aid: string; category: string } }) => {
-    const article = (await getPostById(aid, true)) || []
+    const article = (await getPostById(aid, true)) || {}
     return (
         <Flex className='flex-col flex-wrap justify-start w-full'>
             <ArticleContext data={article} category={category} />
