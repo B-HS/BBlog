9e96cef302c425154fb5d7092a36cbd966a4eca2
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-12 23:13:16 +0900

    Remove old files

--- FRONT/src/lib/constant.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-dayjs.extend(utc)
-
-const DELAY_AMOUNT = 0.15
-const DELAY = (level: number) => level * DELAY_AMOUNT
-const CURRENT_DATE = dayjs().utcOffset(9).format('YYYYMMDDHHmmss')
-
-export { DELAY, DELAY_AMOUNT, CURRENT_DATE }
--- FRONT/src/module/context/loading.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-const LoadingContext = createContext<{ isLoading: boolean; setLoading: Function }>({
-    isLoading: false,
-    setLoading: () => {},
-})
-
-export { LoadingContext }
