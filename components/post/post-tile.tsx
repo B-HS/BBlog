6c47550e59dd8d8097864da3797f78ff45b46f71
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-19 21:24:04 +0900

    Fix font weight for square item

--- components/post/post-tile.tsx
+++ components/post/post-tile.tsx
@@ -11,7 +11,7 @@ const PostTile = async ({ post }: { post: Partial<FrontmatterProps> }) => {
             <Image className='aspect-square object-cover' src={`/image/${thumbnail}`} alt={`${title}-thumbnail`} width={288} height={288} />
             <Link href={`/article/${file?.split('.')[0]}`}>
                 <section className='w-full h-full z-30 absolute top-0 left-0 bg-white dark:bg-black bg-opacity-80 dark:bg-opacity-80 flex flex-col translate-y-72 group-hover:translate-y-0 items-center justify-center transition-all duration-300 cursor-pointer border-2'>
-                    <p>{title}</p>
+                    <p className='text-3xl font-bold'>{title}</p>
                     <p>{dayjs(date).format('YYYY-MM-DD')}</p>
                     <section className='flex flex-wrap gap-2 px-5 justify-center'>
                         {tags
