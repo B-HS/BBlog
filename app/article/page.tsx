d5417df4ca0aaded4bec5a86270c1f48bc70f0a0
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-19 21:19:57 +0900

    Remove unused import

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,6 +1,5 @@
 import Fallback from '@/components/fall-back'
 import PostList from '@/components/post/post-list'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { getFileInfo } from '@/lib/files'
 import { Suspense } from 'react'
 
