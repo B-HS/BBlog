4ab13f2345741d091a7ce83af39a4ce245baa525
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-21 01:31:54 +0900

    Rename comment-fn to crud-comment-fn

--- components/comments/crud-comment-fn.ts
+++ components/comments/crud-comment-fn.ts
@@ -14,17 +14,18 @@ export const addComment = async (formData: FormData) => {
     const { user } = (await supabase.auth.getUser()).data
     const context = formData.get('context') as string
     const post = formData.get('post') as string
+    const upper_id = formData.get('id') as string
     const avatarUrl = user?.user_metadata?.avatar_url || currentURL + '/image/hs-padding.png'
     const username = user?.user_metadata?.name || user?.email || undefined
-    const { data, error } = await supabase
-        .from('comments')
-        .insert({ context, post, user_id: user!.id, upper_id: 0, avatar: avatarUrl, username })
-        .select()
+    const refectoredData = { context, post, user_id: user!.id, upper_id: 0, avatar: avatarUrl, username }
+    if (!!upper_id) {
+        refectoredData.upper_id = parseInt(upper_id)
+    }
+    const { data, error } = await supabase.from('comments').insert(refectoredData).select()
     if (error) {
         console.log(error)
     }
 
     const targetId = (data![0].id || undefined) as CommentProps | undefined
-    console.log('whot', targetId)
     return redirect(currentURL + `/article/${post}?comment=${targetId}#${targetId}`)
 }
