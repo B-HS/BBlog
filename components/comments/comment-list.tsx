8b413d5a6f9e94113e2da3c0e1f05f30bc5c4c18
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-21 01:32:57 +0900

    Implement comment feature

---
+++ components/comments/comment-list.tsx
@@ -0,0 +1,70 @@
+import { User } from '@supabase/supabase-js'
+import { CornerDownRight } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Badge } from '../ui/badge'
+import { CommentProps } from './comments'
+import EditComment from './edit-comment'
+
+interface CommentListProps {
+    comments: CommentProps[]
+    user: User | null
+    post: string
+    upper_id?: number
+    eleDepth?: number
+}
+
+const CommentsList = async ({ comments, user, post, upper_id, eleDepth }: CommentListProps) => {
+    const recursiveComments = (comments: CommentProps[], id?: string | number, depth = 0): CommentProps[] => {
+        const targetComments = comments.filter((ele) => ele.upper_id == id)
+        return targetComments.map((ele) => ({
+            ...ele,
+            depth,
+            children: recursiveComments(comments, ele.id, depth + 1),
+        }))
+    }
+    return (
+        <section className='flex flex-col'>
+            {recursiveComments(comments, upper_id || 0, eleDepth || 0).map((ele, idx) => (
+                <div key={idx} className='flex flex-col'>
+                    <section className='flex w-full items-center justify-start gap-3 py-3' id={ele.id?.toString()}>
+                        {ele.depth !== 0 && (
+                            <>
+                                {Array.from({ length: ele.depth! - 1 }).map((_, idx) => (
+                                    <span className='mx-5 font-bold' key={idx}>
+                                        |
+                                    </span>
+                                ))}
+                                <CornerDownRight className='mx-3.5' key={idx} />
+                            </>
+                        )}
+                        <Avatar className='w-12 h-12'>
+                            <AvatarImage className='m-0 bg-background/15' src={ele.avatar} alt='Avatar' />
+                            <AvatarFallback>{ele.username}</AvatarFallback>
+                        </Avatar>
+                        <section className='flex-1 flex flex-col justify-center px-3'>
+                            <section className='flex gap-1 items-center'>
+                                <span className='font-extrabold'>{ele.username}</span>
+                                {ele.updated_at && (
+                                    <Badge variant={'outline'} className='p-1 py-0.5 h-fit border rounded-none'>
+                                        Edited
+                                    </Badge>
+                                )}
+                            </section>
+                            {ele.deleted ? (
+                                <span className='text-foreground/25 font-bold'> This comment has been deleted </span>
+                            ) : (
+                                <span className='h-8/12'> {ele.context}</span>
+                            )}
+                            <section className='flex gap-1'>
+                                <EditComment comment={ele} user={user} post={post} />
+                            </section>
+                        </section>
+                    </section>
+                    {ele.children && <CommentsList comments={comments} user={user} post={post} upper_id={ele.id} eleDepth={(ele.depth || 0) + 1} />}
+                </div>
+            ))}
+        </section>
+    )
+}
+
+export default CommentsList
--- components/comments/comments.tsx
+++ components/comments/comments.tsx
@@ -1,10 +1,8 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies, headers } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { Button } from '../ui/button'
-import { Input } from '../ui/input'
-import { addComment } from './comment-fn'
+import { Separator } from '../ui/separator'
+import AddCommentInput from './add-comment'
+import CommentsList from './comment-list'
 
 export interface CommentProps {
     id: number
@@ -17,6 +15,8 @@ export interface CommentProps {
     created_at: Date
     updated_at: Date
     deleted: boolean
+    children?: CommentProps[]
+    depth?: number
 }
 
 const Comments = async ({ comments, post }: { comments: CommentProps[]; post: string }) => {
@@ -36,37 +36,10 @@ const Comments = async ({ comments, post }: { comments: CommentProps[]; post: st
                 <span className='text-xl'>Comments</span>
                 <span className='text-foreground/30'>({comments.length})</span>
             </section>
-            {username ? (
-                <section className='flex w-full items-center justify-start gap-3'>
-                    <Avatar className='w-12 h-12'>
-                        <AvatarImage className='m-0 bg-background/15' src={avatarUrl} alt='Avatar' />
-                        <AvatarFallback>{user?.user_metadata?.name || user?.email}</AvatarFallback>
-                    </Avatar>
-                    <form className='flex-1 flex gap-1' action={addComment}>
-                        <Input type='text' id='context' name='context' />
-                        <input type='hidden' id='post' name='post' value={post} />
-                        <Button variant='secondary' type='submit'>
-                            Submit
-                        </Button>
-                    </form>
-                </section>
-            ) : (
-                <p className='text-foreground/30'>Please login to comment</p>
-            )}
-            <section className='flex flex-col'>
-                {comments.map((ele, idx) => (
-                    <span key={idx}>
-                        {/* 사용자 이름 */}
-                        <p>{ele.username}</p>
-                        {/* 댓글 내용 */}
-                        <p>{ele.context}</p>
-                        {/* 내가 쓴 덧글인지 확인하는 로직 */}
-                        <p>{ele.user_id === user?.id ? 'me' : 'not me'}</p>
-                        {/* 아바타 주소 */}
-                        <p>{ele.avatar}</p>
-                    </span>
-                ))}
-            </section>
+            <AddCommentInput username={username} user={user} avatarUrl={avatarUrl} post={post} />
+            <Separator className='my-3 h-2' />
+            <CommentsList comments={comments} user={user} post={post} />
+            <Separator className='my-3 h-2' />
         </section>
     )
 }
