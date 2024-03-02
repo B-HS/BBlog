86c5d580e888e4b8d023516bbe2c0cef7d046b18
Hyunseok Byun <gumyoincirno@gmail.com>
2024-03-02 20:50:20 +0900

    Fix article tile text alignment

--- components/post/post-tile.tsx
+++ components/post/post-tile.tsx
@@ -17,7 +17,7 @@ const PostTile = async ({ post }: { post: Partial<FrontmatterProps> }) => {
                 height={288}
             />
             <Link href={`/article/${file?.split('.')[0]}`}>
-                <section className='w-full h-full z-30 absolute top-0 left-0 bg-white dark:bg-black bg-opacity-80 dark:bg-opacity-80 flex flex-col translate-y-72 group-hover:translate-y-0 items-center justify-center transition-all duration-300 cursor-pointer border-2'>
+                <section className='w-full h-full z-30 absolute top-0 left-0 bg-white dark:bg-black bg-opacity-80 dark:bg-opacity-80 flex flex-col translate-y-72 group-hover:translate-y-0 items-center justify-center transition-all duration-300 cursor-pointer border-2 text-center'>
                     <p className='text-3xl font-bold'>{title}</p>
                     <p>{dayjs(date).format('YYYY-MM-DD')}</p>
                     <section className='flex flex-wrap gap-2 px-5 justify-center'>
