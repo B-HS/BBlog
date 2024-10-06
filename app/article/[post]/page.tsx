c6ec4f8768022e0449cc736edf0648aa2a7d2d00
Hyunseok Byun <gumyoincirno@gmail.com>
2024-10-06 16:57:10 +0900

    Update post detail page

--- app/article/[post]/page.tsx
+++ app/article/[post]/page.tsx
@@ -1,21 +1,28 @@
+import { ArticleDetail } from '@entities/article'
 import { CustomMdx, MdxPage } from '@features/mdx'
 import { Comments } from '@features/mdx/comments'
+import { useCurrentPath } from '@shared/hooks'
 import { markdownToText } from '@shared/utils'
-import fs from 'fs'
 import { Metadata } from 'next'
-import { MDXRemoteProps } from 'next-mdx-remote/rsc'
-import path from 'path'
+import { Fragment } from 'react'
 
 export const generateMetadata = async ({ params }: { params: { post: string } }): Promise<Metadata> => {
-    const source = (await getPostSource(params.post)) as MDXRemoteProps['source']
-    const { frontmatter } = await CustomMdx({ source })
-    const context = (markdownToText(source?.toString().slice(0, 250)) || frontmatter.title || '')?.replace(/<\/?[^>]+(>|$)/g, '') + '...'
+    const { url } = useCurrentPath()
+    const fetchedData = await fetch(`${url}/api/article/${params.post}`).then((res) => res.json())
+    const source = fetchedData as ArticleDetail
+    const frontmatter = {
+        title: source?.post?.title || '',
+        tags: source?.tags || [],
+        date: source?.post?.createdAt || '',
+        category: source?.category || '',
+        thumbnail: '',
+        viewCnt: source?.post?.views || '',
+    }
+    const context =
+        (markdownToText(source?.post.description.toString().slice(0, 250)) || frontmatter.title || '')?.replace(/<\/?[^>]+(>|$)/g, '') + '...'
 
     return {
-        title: {
-            template: '%s | HS',
-            default: frontmatter.title || 'Article',
-        },
+        title: frontmatter.title,
         description: context || `Article | ${process.env.SITE_NAME}`,
         keywords: frontmatter.tags?.join(', ') || `Article | ${process.env.SITE_NAME}`,
         openGraph: {
@@ -42,37 +49,25 @@ export const generateMetadata = async ({ params }: { params: { post: string } })
     }
 }
 
-const getPostSource = async (postName: string) => {
-    const filePath = path.join(process.cwd(), 'public', 'post', `${postName}.mdx`)
-    try {
-        return await fs.promises.readFile(filePath, 'utf8')
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const getCommentList = async (postName: string) => {
-    path.join(process.cwd(), 'public', 'post', `${postName}.mdx`)
-    return Promise.resolve([])
-}
-
-const manageViewCnt = async (postName: string) => {
-    path.join(process.cwd(), 'public', 'post', `${postName}.mdx`)
-    return Promise.resolve('0')
-}
-
 const RemoteMdxPage = async ({ params }: { params: { post: string } }) => {
-    const source = (await getPostSource(params.post)) as MDXRemoteProps['source']
-    const viewCnt = await manageViewCnt(params.post)
-    const comments = await getCommentList(params.post)
-
-    const { content, frontmatter } = await CustomMdx({ source })
+    const { url } = useCurrentPath()
+    const fetchedData = await fetch(`${url}/api/article/${params.post}`).then((res) => res.json())
+    const source = fetchedData as ArticleDetail
+    const frontmatter = {
+        title: source?.post?.title || '',
+        tags: source?.tags.at(0)?.split(',') || [],
+        date: source?.post?.createdAt.toString() || '',
+        category: source?.category || '',
+        thumbnail: '',
+        viewCnt: String(source?.post?.views) || '',
+    }
+    const { content } = await CustomMdx({ source: source.post.description })
 
     return (
-        <>
-            <MdxPage content={content} frontmatter={{ ...frontmatter, viewCnt }} />
-            <Comments comments={comments} post={params.post} />
-        </>
+        <Fragment>
+            <MdxPage content={content} frontmatter={{ ...frontmatter }} />
+            <Comments comments={source.comments || []} post={params.post} />
+        </Fragment>
     )
 }
 export default RemoteMdxPage
--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,5 +1,130 @@
+'use client'
+
+import { Article } from '@entities/article'
+import { Category } from '@entities/category'
+import { Badge } from '@shared/ui/badge'
+import { Button } from '@shared/ui/button'
+import { Skeleton } from '@shared/ui/skeleton'
+import { cn } from '@shared/utils'
+import { ArticleList } from '@widgets/article'
+import { Fragment, useCallback, useEffect, useRef, useState } from 'react'
+
+const useArticles = (selectedCategory: Category | null, page: number, isCategoryLoaded: boolean) => {
+    const [articles, setArticles] = useState<Article[]>([])
+    const [hasNext, setHasNext] = useState(true)
+    const [isArticleLoaded, setIsArticleLoaded] = useState(false)
+    const isFirstFetch = useRef(true) // This will track if it's the first load
+
+    useEffect(() => {
+        if (isFirstFetch.current) {
+            isFirstFetch.current = false // Prevents further fetches on initial load
+            return
+        }
+
+        const queryBuilder = new URLSearchParams()
+        queryBuilder.append('page', page.toString())
+        if (selectedCategory) {
+            queryBuilder.append('categoryId', selectedCategory.categoryId.toString())
+        }
+
+        fetch(`/api/article?${queryBuilder.toString()}`)
+            .then((res) => res.json())
+            .then((data) => {
+                const { posts } = data as { posts: Article[] }
+                if (posts.length > 0) {
+                    setArticles((prev) => [...prev, ...posts])
+                } else {
+                    setHasNext(false)
+                }
+            })
+            .finally(() => setIsArticleLoaded(true))
+    }, [page, selectedCategory])
+
+    return { articles, hasNext, isArticleLoaded, setArticles, setIsArticleLoaded }
+}
+
+const useCategories = () => {
+    const [categories, setCategories] = useState<Category[]>([])
+    const [isCategoryLoaded, setIsCategoryLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch('/api/category')
+            .then((res) => res.json())
+            .then((data) => {
+                const { categories } = data
+                setCategories(categories || [])
+            })
+            .finally(() => setIsCategoryLoaded(true))
+    }, [])
+
+    return { categories, isCategoryLoaded }
+}
+
+const SkeletonLoader = ({ count, className, isFlex }: { count: number; className: string; isFlex?: boolean }) => (
+    <section className={cn(isFlex ? 'flex gap-2' : 'grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3')}>
+        {Array.from({ length: count }).map((_, i) => (
+            <Skeleton key={i} className={className} />
+        ))}
+    </section>
+)
+
 const ArticleListPage = () => {
-    return <section>LIST</section>
+    const [selectedCategory, setSelectedCategory] = useState<Category | null>(null)
+    const [page, setPage] = useState(1)
+
+    const { categories, isCategoryLoaded } = useCategories()
+    const { articles, hasNext, isArticleLoaded, setArticles, setIsArticleLoaded } = useArticles(selectedCategory, page, isCategoryLoaded)
+
+    const handleCategorySelect = useCallback((category: Category | null) => {
+        setIsArticleLoaded(false)
+        setArticles([])
+        setPage(1)
+        setSelectedCategory(category)
+    }, [])
+
+    return (
+        <section className='size-full flex flex-col gap-10 p-2'>
+            <section className='flex flex-col gap-2 p-2'>
+                <p className='font-bold text-2xl'>Articles</p>
+                <section className='flex gap-2 overflow-scroll'>
+                    {!isCategoryLoaded ? (
+                        <SkeletonLoader count={5} className='h-[22px] w-10' isFlex />
+                    ) : (
+                        <Fragment>
+                            <Badge
+                                onClick={() => handleCategorySelect(null)}
+                                className='rounded-sm'
+                                variant={!selectedCategory?.categoryId ? 'default' : 'outline'}>
+                                All
+                            </Badge>
+                            {categories.map((category) => (
+                                <Badge
+                                    onClick={() => handleCategorySelect(category)}
+                                    className='rounded-sm'
+                                    key={category.categoryId}
+                                    variant={selectedCategory?.categoryId === category.categoryId ? 'default' : 'outline'}>
+                                    {category.category}
+                                </Badge>
+                            ))}
+                        </Fragment>
+                    )}
+                </section>
+
+                {!isArticleLoaded ? (
+                    <SkeletonLoader count={6} className='sm:w-90 h-[119px]' />
+                ) : (
+                    <ArticleList articles={articles} category={categories} />
+                )}
+
+                <Button
+                    onClick={() => hasNext && setPage((page) => page + 1)}
+                    variant={hasNext ? 'outline' : 'ghost'}
+                    className={cn(!hasNext && 'hover:bg-background cursor-default')}>
+                    {hasNext ? 'Load more' : 'No more articles.'}
+                </Button>
+            </section>
+        </section>
+    )
 }
 
 export default ArticleListPage
