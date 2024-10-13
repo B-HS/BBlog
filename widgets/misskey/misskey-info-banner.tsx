32e72add891fd3a9cbd14da2290566469e19d2f8
Hyunseok Byun <gumyoincirno@gmail.com>
2024-10-13 18:02:48 +0900

    Replace image logic to using next/image

--- widgets/misskey/misskey-info-banner.tsx
+++ widgets/misskey/misskey-info-banner.tsx
@@ -1,14 +1,11 @@
+import Image from 'next/image'
 import { MisskeyInfo } from '@entities/misskey'
 
 export const MisskeyInfoBanner = ({ misskeyInfo }: { misskeyInfo: MisskeyInfo }) => {
     return (
-        <section
-            className='h-64 bg-gradient-to-b from-neutral-50/10 to-neutral-600/10 hover:h-72 transition-all'
-            style={{
-                backgroundImage: `url(${misskeyInfo.bannerUrl})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}
-        />
+        <section className='relative h-64 hover:h-72 transition-all'>
+            <Image src={misskeyInfo.bannerUrl} alt='Misskey Banner' fill className='pointer-events-none select-none object-cover' priority={true} />
+            <div className='absolute inset-0 bg-gradient-to-b from-neutral-50/10 to-neutral-600/10' />
+        </section>
     )
 }
