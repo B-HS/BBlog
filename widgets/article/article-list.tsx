d3d1740ab50ddf5334b5c4f5b44ed29b27ab2305
Hyunseok Byun <gumyoincirno@gmail.com>
2024-10-19 22:46:23 +0900

    Set key for mapped article component

--- widgets/article/article-list.tsx
+++ widgets/article/article-list.tsx
@@ -6,8 +6,12 @@ export const ArticleList = ({ articles, category }: { articles?: Article[]; cate
     if (articles?.length === 0) return <section>No articles found</section>
     return (
         <section className='grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3'>
-            {articles?.filter((article) => article.isNotice).map((article) => ArticleCard({ article, category }))}
-            {articles?.filter((article) => !article.isNotice).map((article) => ArticleCard({ article, category }))}
+            {articles
+                ?.filter((article) => article.isNotice)
+                .map((article) => <ArticleCard key={article.postId} article={article} category={category} />)}
+            {articles
+                ?.filter((article) => !article.isNotice)
+                .map((article) => <ArticleCard key={article.postId} article={article} category={category} />)}
         </section>
     )
 }
