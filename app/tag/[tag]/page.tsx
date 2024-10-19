58226f734f4d263782aa45924ef2dd8db4624581
Hyunseok Byun <gumyoincirno@gmail.com>
2024-10-19 22:45:46 +0900

    Replace encoded text to decoded text

--- app/tag/[tag]/page.tsx
+++ app/tag/[tag]/page.tsx
@@ -7,7 +7,8 @@ const TagSearchPage = async ({ params }: { params: { tag: number | string } }) =
         <section className='size-full flex flex-col gap-10 p-2'>
             <section className='flex flex-col gap-3 p-2'>
                 <p className='font-bold text-xl'>
-                    Search result for <span className='underline underline-offset-8 text-2xl'>{params.tag}</span> tag
+                    Search result for{' '}
+                    <span className='underline underline-offset-8 text-2xl'>{decodeURIComponent((params.tag + '').replace(/\+/g, '%20'))}</span> tag
                 </p>
                 <ArticleList articles={articlesData?.posts} category={articlesData.categories} />
             </section>
