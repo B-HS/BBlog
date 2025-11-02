ec9998111759e7efb1960034cf3c0746d11b0539
Hyunseok Byun <gumyoincirno@gmail.com>
2025-11-02 15:27:36 +0900

    Implement ga4 and and workflows file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import { VirtualScroll } from '@features/theme/virtual-scroll'
 import { TanstackQueryProvider } from '@lib/providers/tanstack-query-provider'
 import { ThemeProvider } from '@lib/providers/theme-provider'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Toaster } from '@ui/sonner'
+import { GoToTop } from '@widgets/layout/go-to-top'
 import { FC, PropsWithChildren } from 'react'
 import './globals.css'
-import { GoToTop } from '@widgets/layout/go-to-top'
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
     return (
         <html lang='ko' data-scroll-behavior='smooth' suppressHydrationWarning>
+            <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ''} />
             <body className='antialiased relative'>
                 <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
                     <TanstackQueryProvider>{children}</TanstackQueryProvider>
