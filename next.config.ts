b7f519cfc797e205081c8510adda181170743f38
Hyunseok Byun <gumyoincirno@gmail.com>
2025-11-03 08:39:43 +0900

    Fix nextjs config for not doing streaming the metadata

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+    // https://nextjs.org/docs/app/api-reference/functions/generate-metadata#streaming-metadata
+    // 굳이 streaming metadata 사용하지 않아도 됨, 어차피 사이트가 그리 크지 않고 캐싱될거임
+    htmlLimitedBots: /.*/,
     cacheComponents: true,
     reactCompiler: true,
     output: 'standalone',
