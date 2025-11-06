edb4d27fce778256655d1ca389b48e4be5179233
Hyunseok Byun <gumyoincirno@gmail.com>
2025-11-06 16:09:14 +0900

    Add baseURL to auth configuration

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -4,6 +4,7 @@ import { db } from '@db/db'
 import { admin } from 'better-auth/plugins'
 
 export const auth = betterAuth({
+    baseURL: process.env.SITE_URL,
     database: drizzleAdapter(db, {
         provider: 'mysql',
     }),
